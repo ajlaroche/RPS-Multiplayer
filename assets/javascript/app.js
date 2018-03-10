196b339326e10c1640eda485aed1a14496157a4d
Antony Laroche <antony@larochefamily.net>
2018-03-09 23:32:07 -0600

    added database functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,20 +7,54 @@ $(document).ready(function () {
         projectId: "rps-multiplayer-fe4e4",
         storageBucket: "",
         messagingSenderId: "651993058334"
-      };
-      firebase.initializeApp(config);
+    };
+    firebase.initializeApp(config);
 
 
-    var guesses = ["r", "p", "s"]
     var playerOneGuess = "";
     var playerTwoGuess = "";
-    var dbPlayerOneChoice=firebase.database().ref().child("playerOneChoice");
-    var resultTest = "";
+    var refDatabase = firebase.database().ref();
+    var dbPlayerOneChoice = firebase.database().ref().child("playerOneChoice");
+    var dbPlayerTwoChoice = firebase.database().ref().child("playerTwoChoice");
+   var whoAmI;
+    var userProfile = { name: "", wins: 0, losses: 0, choice: "" };
 
-      dbPlayerOneChoice.on("value", snap =>$("#playerOneChoice").text(snap.val()));
+    refDatabase.child("playerOne").remove();
+    refDatabase.child("playerTwo").remove();
 
-    $("#playerOne").on("click",".selection", function(){
+    //   dbPlayerOneChoice.on("value", snap =>$("#playerOneChoice").text(snap.val()));
+
+    dbPlayerOneChoice.on("value", function (data) {
+        console.log(data.val())
+        $("#playerOneChoice").text(data.val());
+    })
+
+    dbPlayerTwoChoice.on("value", function (data) {
+        console.log(data.val())
+        $("#playerTwoChoice").text(data.val());
+    })
+
+
+
+    $("#SaveNameButton").on("click", function () {
+
+        if ($("#playerName").val() !== "") {
+            userProfile.name = $("#playerName").val().trim();
+            if (false) {  //Need to figure out if player one already exists
+                refDatabase.child("playerTwo").set(userProfile);
+                console.log("playerOne Detected");
+                whoAmI="playerTwo";
+            } else {
+                refDatabase.child("playerOne").set(userProfile);
+                console.log(userProfile);
+                whoAmI=eval("playerOne");
+            }
+        }
+    })
+
+    $("#playerOne").on("click", ".selection", function () {
         console.log($(this).attr("data-choice"));
-        playerOneGuess= $(this).attr("data-choice");
+        playerOneGuess = $(this).attr("data-choice");
+        refDatabase.child("/playerOne/"+"choice").set(playerOneGuess);
     })
 })
