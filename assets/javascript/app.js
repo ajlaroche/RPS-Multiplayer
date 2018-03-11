7445e7acdf7ddfba0054e25499b1d1bae4fc2545
Antony Laroche <antony@larochefamily.net>
2018-03-10 23:53:50 -0600

    updated database parameters, game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,48 +13,146 @@ $(document).ready(function () {
 
     var playerOneGuess = "";
     var playerTwoGuess = "";
-    var refDatabase = firebase.database().ref();
-    var dbPlayerOneChoice = firebase.database().ref().child("playerOneChoice");
-    var dbPlayerTwoChoice = firebase.database().ref().child("playerTwoChoice");
-   var whoAmI;
+    var refDatabase = firebase.database();
+    var whoAmI = "";
     var userProfile = { name: "", wins: 0, losses: 0, choice: "" };
+    var dbPlayerOneChoice = "";
+    var dbPlayerTwochoice = "";
+    var playerOneWins = 0;
+    var playerOneLoss = 0;
+    var playerTwoWins = 0;
+    var playerTwoLoss = 0;
 
-    refDatabase.child("playerOne").remove();
-    refDatabase.child("playerTwo").remove();
+    refDatabase.ref("/playerOne/" + "choice").set("");
+    refDatabase.ref("/playerTwo/" + "choice").set("");
+    refDatabase.ref("/playerOne/" + "wins").set(0);
+    refDatabase.ref("/playerTwo/" + "wins").set(0);
+    refDatabase.ref("/playerOne/" + "losses").set(0);
+    refDatabase.ref("/playerTwo/" + "losses").set(0);
 
-    //   dbPlayerOneChoice.on("value", snap =>$("#playerOneChoice").text(snap.val()));
+    var otherUser = firebase.auth().currentUser;
+    setTimeout(function () { console.log(otherUser) }, 5000);
 
-    dbPlayerOneChoice.on("value", function (data) {
-        console.log(data.val())
-        $("#playerOneChoice").text(data.val());
+    //check if I'm player one or two
+    refDatabase.ref().once("value", function (snapshot) {
+        var enteredData = snapshot.val();
+        if (enteredData.playerOne.name === "") {
+            whoAmI = "playerOne";
+        } else {
+            whoAmI = "playerTwo";
+        }
+        console.log(enteredData.playerOne);
+        console.log(whoAmI);
     })
 
-    dbPlayerTwoChoice.on("value", function (data) {
-        console.log(data.val())
-        $("#playerTwoChoice").text(data.val());
+    //listen for user picks
+    refDatabase.ref().on("value", function (snapshot) {
+        dbPlayerOneChoice = snapshot.val().playerOne.choice;
+        dbPlayerTwochoice = snapshot.val().playerTwo.choice;
+        playerOneWins = snapshot.val().playerOne.wins;
+        // playerOneLoss = snapshot.val().playerOne.losses;
+        playerTwoWins = snapshot.val().playerTwo.wins;
+        // playerTwoLoss = snapshot.val().playerTwo.losses;
+        $("#playerOneChoice").text(dbPlayerOneChoice);
+        $("#playerTwoChoice").text(dbPlayerTwochoice);
+        console.log(snapshot.val());
+
     })
 
-
-
+    //enter user names
     $("#SaveNameButton").on("click", function () {
 
         if ($("#playerName").val() !== "") {
             userProfile.name = $("#playerName").val().trim();
-            if (false) {  //Need to figure out if player one already exists
-                refDatabase.child("playerTwo").set(userProfile);
+            if (whoAmI === "playerTwo") {  //Need to figure out if player one already exists
+                refDatabase.ref("playerTwo").set(userProfile);
                 console.log("playerOne Detected");
-                whoAmI="playerTwo";
             } else {
-                refDatabase.child("playerOne").set(userProfile);
+                refDatabase.ref("playerOne").set(userProfile);
                 console.log(userProfile);
-                whoAmI=eval("playerOne");
             }
         }
     })
+    $("#playerName").keypress(function (e) {
+        if (e.keyCode == 13)
+            $("#SaveNameButton").click();
+    });
+
+
 
     $("#playerOne").on("click", ".selection", function () {
-        console.log($(this).attr("data-choice"));
-        playerOneGuess = $(this).attr("data-choice");
-        refDatabase.child("/playerOne/"+"choice").set(playerOneGuess);
+        if (whoAmI === "playerOne") {
+            console.log($(this).attr("data-choice"));
+            playerOneGuess = $(this).attr("data-choice");
+            refDatabase.ref("/playerOne/" + "choice").set(playerOneGuess);
+            findWinner();
+
+        }
     })
+    $("#playerTwo").on("click", ".selection", function () {
+        if (whoAmI === "playerTwo") {
+            console.log($(this).attr("data-choice"));
+            playerTwoGuess = $(this).attr("data-choice");
+            refDatabase.ref("/playerTwo/" + "choice").set(playerTwoGuess);
+            findWinner();
+        }
+    })
+
+    //Find winner of game
+    var playerOneTest = "";
+    var playerTwoTest = "";
+    var resultTest = "";
+    var winner;
+
+
+    function findWinner() {
+        if (dbPlayerOneChoice !== "" && dbPlayerTwochoice !== "") {
+            switch (dbPlayerOneChoice) {
+                case "rock":
+                    playerOneTest = "r";
+                    break;
+                case "paper":
+                    playerOneTest = "p";
+                    break;
+                case "scissors":
+                    playerOneTest = "s";
+            }
+            switch (dbPlayerTwochoice) {
+                case "rock":
+                    playerTwoTest = "r";
+                    break;
+                case "paper":
+                    playerTwoTest = "p";
+                    break;
+                case "scissors":
+                    playerTwoTest = "s";
+            }
+            resultTest = playerOneTest + playerTwoTest;
+            console.log(resultTest);
+            if (resultTest === "rs" || resultTest === "pr" || resultTest === "sp") {
+                winner = "playerOne";
+                playerOneWins++;
+                playerTwoLoss++;
+                refDatabase.ref("/playerOne/" + "wins").set(playerOneWins);
+                refDatabase.ref("/playerTwo/" + "losses").set(playerTwoLoss);
+            } else if (resultTest === "rr" || resultTest === "ss" || resultTest === "pp") {
+                winner = "It's a Tie!"
+
+            } else {
+                winner = "playerTwo";
+                playerTwoWins++;
+                playerOneLoss++;
+                refDatabase.ref("/playerTwo/" + "wins").set(playerTwoWins);
+                refDatabase.ref("/playerOne/" + "losses").set(playerOneLoss);
+
+            }
+            refDatabase.ref("/playerOne/" + "choice").set("");
+            refDatabase.ref("/playerTwo/" + "choice").set("");
+
+            console.log(playerOneWins, playerOneLoss)
+
+
+            console.log(winner);
+        }
+    }
 })
